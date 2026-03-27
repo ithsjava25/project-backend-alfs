8072c955efcdff95e596c25fdc17763db2268885
Simon Forsberg <simon.co.forsberg@gmail.com>
2026-03-27 10:09:34 +0100

    Initial commit

---
+++ alfs/src/test/java/org/example/alfs/AlfsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.alfs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlfsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

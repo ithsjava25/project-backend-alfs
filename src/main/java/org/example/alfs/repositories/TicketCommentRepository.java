db02b93c49b2be95dd313dbbed5ae7ee2f3e0f16
Simon Forsberg <simon.co.forsberg@gmail.com>
2026-03-31 14:14:21 +0200

    Add `TicketCommentRepository` to support TicketComment-related queries

---
+++ src/main/java/org/example/alfs/repositories/TicketCommentRepository.java
@@ -0,0 +1,15 @@
+package org.example.alfs.repositories;
+
+import org.example.alfs.entities.TicketComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TicketCommentRepository extends JpaRepository<TicketComment, Long> {
+
+    // Ladda alla kommentarer i ett fall, äldst först
+    List<TicketComment> findByTicketIdOrderByCreatedAtAsc(Long ticketId);
+
+    // Ladda interna meddelanden, för utredare/admins
+    List<TicketComment> findByTicketIdAndIsInternalNote(Long ticketId, boolean isInternalNote);
+}

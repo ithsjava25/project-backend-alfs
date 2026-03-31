a875aa55bfca8121c27f47bd8aa5aa249f38fdee
Fiona Friberg <fionafriberg@outlook.com>
2026-03-31 14:15:13 +0200

    Create TicketCreateDTO

---
+++ src/main/java/org/example/alfs/dto/TicketCreateDTO.java
@@ -0,0 +1,23 @@
+package org.example.alfs.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+/*
+ * Reporter creates ticket
+ * The reporter provides the initial information describing the incident.
+ *
+ * The reporter cannot modify the ticket after creation. Any additional
+ * information must be provided through comments.
+ */
+
+@Data
+public class TicketCreateDTO {
+
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String description;
+}

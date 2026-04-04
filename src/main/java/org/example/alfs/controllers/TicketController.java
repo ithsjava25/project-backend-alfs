d27fb3fb048759589481fc6e56101839face27e5
Fiona Friberg <fionafriberg@outlook.com>
2026-04-04 16:26:03 +0200

    add viewTicketByToken

--- src/main/java/org/example/alfs/controllers/TicketController.java
+++ src/main/java/org/example/alfs/controllers/TicketController.java
@@ -2,15 +2,13 @@ package org.example.alfs.controllers;
 
 import jakarta.validation.Valid;
 import org.example.alfs.dto.ticket.TicketCreateDTO;
+import org.example.alfs.dto.ticket.TicketViewDTO;
 import org.example.alfs.entities.Ticket;
 import org.example.alfs.services.TicketService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping
@@ -45,7 +43,16 @@ public class TicketController {
 
     }
 
-    //view ticket
+    //view ticket by token
+
+    @GetMapping("/view/{token}")
+    public String viewTicketByToken(@PathVariable String token, Model model) {
+
+        TicketViewDTO ticket = ticketService.getTicketByToken(token);
+        model.addAttribute("ticket", ticket);
+
+        return "view";
+    }
     //assign ticket
     //update status
     //create comment 

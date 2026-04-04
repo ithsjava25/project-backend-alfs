343ea3ca9d98fc81dfe530ff84620891a1e06edd
Fiona Friberg <fionafriberg@outlook.com>
2026-04-04 11:04:24 +0200

    Create TicketMapper, add entityToViewDTO

---
+++ src/main/java/org/example/alfs/mapper/TicketMapper.java
@@ -0,0 +1,28 @@
+package org.example.alfs.mapper;
+
+import org.example.alfs.dto.ticket.TicketViewDTO;
+import org.example.alfs.entities.Ticket;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TicketMapper {
+
+    public TicketViewDTO entityToViewDTO(Ticket ticket){
+
+        TicketViewDTO ticketViewDTO = new TicketViewDTO();
+
+        ticketViewDTO.setId(ticket.getId());
+        ticketViewDTO.setTitle(ticket.getTitle());
+        ticketViewDTO.setDescription(ticket.getDescription());
+        ticketViewDTO.setStatus(ticket.getStatus());
+        ticketViewDTO.setCreatedAt(ticket.getCreatedAt());
+
+        if (ticket.getInvestigator() != null) {
+            ticketViewDTO.setAssignedInvestigatorId(ticket.getInvestigator().getId());
+        }
+
+        return ticketViewDTO;
+    }
+
+
+}

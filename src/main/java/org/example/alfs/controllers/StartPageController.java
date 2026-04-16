6f3734024ccee12386fd87ebf358806e600cf39a
Adam Ottosson <addeecsgo@gmail.com>
2026-04-16 11:22:33 +0200

    Add Model parameter to start page handler

--- src/main/java/org/example/alfs/controllers/StartPageController.java
+++ src/main/java/org/example/alfs/controllers/StartPageController.java
@@ -1,13 +1,14 @@
 package org.example.alfs.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class StartPageController {
 
     @GetMapping("/")
-    public String startPage(){
+    public String startPage(Model model){
         return "startPage";
     }
 }

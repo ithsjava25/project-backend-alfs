c0c718e873153fce08c26b33609dbee2f8ab2ed4
Fiona Friberg <fionafriberg@outlook.com>
2026-04-17 11:50:09 +0200

    Create ErrorController and initial code

---
+++ src/main/java/org/example/alfs/controllers/ErrorController.java
@@ -0,0 +1,18 @@
+package org.example.alfs.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class ErrorController {
+
+    @GetMapping("/403")
+    public String forbidden(Model model) {
+        model.addAttribute("status", 403);
+        model.addAttribute("error", "Access denied");
+        return "error";
+    }
+}

c1721b565dc8ce371489f3c714a2d892e862fbf8
Adam Ottosson <addeecsgo@gmail.com>
2026-04-20 11:19:59 +0200

    feat(users): add service method for loading investigators

--- src/main/java/org/example/alfs/services/UserService.java
+++ src/main/java/org/example/alfs/services/UserService.java
@@ -3,9 +3,11 @@ package org.example.alfs.services;
 import org.example.alfs.entities.User;
 import org.example.alfs.enums.Role;
 import org.example.alfs.repositories.UserRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class UserService {
 
     private final UserRepository userRepository;

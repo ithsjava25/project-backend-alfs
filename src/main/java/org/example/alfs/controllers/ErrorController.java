2f5f8f7218ba1ee56bf65b659087e417b9badac2
Fiona Friberg <fionafriberg@outlook.com>
2026-04-20 10:58:12 +0200

    Add base path "error"

--- src/main/java/org/example/alfs/controllers/ErrorController.java
+++ src/main/java/org/example/alfs/controllers/ErrorController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping
+@RequestMapping("/error")
 public class ErrorController {
 
     @GetMapping("/403")
